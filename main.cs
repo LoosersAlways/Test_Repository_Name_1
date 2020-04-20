7bb001a8718b8e9eccd7f950bf63e4588533932b
NotTheSharpestToolInAShed <44844444+NotTheSharpestToolInAShed@users.noreply.github.com>
2020-04-20 18:50:12 +0300

    update main.cs
    Update. What a grand and intoxicating innocence.

--- main.cs
+++ main.cs
@@ -2,10 +2,11 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World")
-    int a = 4, b = 3
+    Console.WriteLine ("BETTER LIVE BETTER DIE");
+    int a = 4, b = 3;
     int c = a + b;
-    Console.WriteLine(c)
+    Console.WriteLine(c);
+    
     
   }
 }
