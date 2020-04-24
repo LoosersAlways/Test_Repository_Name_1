d10f0a9e3cd8a41644aa4cfa2582843d488c28fc
NotTheSharpestToolInAShed <44844444+NotTheSharpestToolInAShed@users.noreply.github.com>
2020-04-24 18:18:38 +0300

    main.cs

---
+++ main1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("BETTER LIVE BETTER DIE");
+    int a = 4, b = 3;
+    int c = a + b;
+    Console.WriteLine(c);
+    
+    
+  }
+}
